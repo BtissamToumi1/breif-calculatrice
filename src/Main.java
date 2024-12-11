4ddaaf4e55a7cad661acf970d3cd04fa5291ce4f
Btissam <btissamtoumi5@email.com>
2024-12-11 15:32:26 +0100

    Fonction de Multiplication

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+        static int  a;
         //fonction de l'addition
-        static void Addition(int x, int y) {
-            System.out.println("la somme est :" + (x + y));
-        }
+         void Addition(int x, int y) {
+             System.out.println("la somme est :" + (x + y));
+         }
         static void Subtraction(int x, int y)  {
             System.out.println("la subtraction est :" + (x - y));
-    }
+        }
+        static void Multiplication(int x, int y) {
+            System.out.println("la multiplication est :" + (x * y));
+        }
         public static void main(String[] args) {
         }
     }
